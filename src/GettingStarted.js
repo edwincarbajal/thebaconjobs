65938eb3082af43ea03f6e3517747cbc7c765f0e
edwincarbajal <exaviercarbajal@gmail.com>
2017-08-18 01:59:08 -0400

    Component that will hold log in/sign up

---
+++ src/GettingStarted.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class GettingStarted extends Component {
+  render() {
+    return(
+      <div className="row">
+        <div className="col-md-6 text-center">
+          <h2>I want to sign up</h2>
+          <p>I want to shape the future by joining one of the fastest growing technology startups on TheBacon.</p>
+        </div>
+        <div className="col-md-6 text-center">
+          <h2>I want to log in</h2>
+          <p>I want to continue me job search of post a new job posting.</p>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default GettingStarted;
