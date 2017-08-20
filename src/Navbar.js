1177a513c97ef4380a72d544f7d72592891a57d5
edwincarbajal <exaviercarbajal@gmail.com>
2017-08-19 22:49:10 -0400

    Modified navbar

--- src/Navbar.js
+++ src/Navbar.js
@@ -7,13 +7,7 @@ const Navbar = () => {
       <Link to="/" className="navbar-brand">theBaconJobs</Link>
       <ul className="nav justify-content-end">
         <li className="nav-item">
-          <a className="nav-link active" href="#">How it Works</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#">Why List Your Business</a>
-        </li>
-        <li className="nav-item">
-          <button type="button" className="btn btn-warning">Post Job</button>
+          <Link to="/job/new" type="button" className="btn btn-warning">Post Job</Link>
         </li>
       </ul>
     </nav>
