2298965230dce1f4c5931611e3c9fcf2084b26c4
edwincarbajal <exaviercarbajal@gmail.com>
2017-08-20 04:23:09 -0400

    Custom styling for index page and responsive

--- src/Welcome.js
+++ src/Welcome.js
@@ -4,16 +4,14 @@ import styles from './Welcome.css';
 
 const Welcome = props => {
   return (
-    <div className="row">
+    <div className="row align-items-center">
       <div id="welcome-container" className="col-12">
         <h6 className="text-uppercase text-secondary text-left">
           We help you bring home the bacon
         </h6>
         <h1 className="text-left">
-          The Bacon brings <span className="font-weight-bold">
-            new roles
-          </span>{' '}
-          to the table because,<br /> we're{' '}
+          TheBacon brings <span className="font-weight-bold">new roles</span> to
+          the table because,<br /> we're{' '}
           <span className="font-weight-bold">better</span> when we work{' '}
           <span className="font-weight-bold">together</span>.
         </h1>
@@ -22,7 +20,7 @@ const Welcome = props => {
             <button
               type="button"
               onClick={props.handleGetStartedClick}
-              className="btn btn-light btn-lg"
+              className="btn btn-lg"
             >
               Get Started
             </button>
