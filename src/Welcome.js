bbac600889ccad409716ba176c0fe37f9aa5e048
edwincarbajal <exaviercarbajal@gmail.com>
2017-08-19 22:59:22 -0400

    Update component

--- src/Welcome.js
+++ src/Welcome.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const Welcome = () => {
+const Welcome = (props) => {
   return(
     <div className="row">
       <div className="col-md-7">
@@ -9,7 +9,7 @@ const Welcome = () => {
         <p className="lead text-left">The Bacon brings <span className="font-weight-bold">new roles</span> to the table, because<br/> we're <span className="font-weight-bold">better</span> when we work <span className="font-weight-bold">together</span>.</p>
         <p>
           <Link to="/getting-started">
-            <button type="button" onClick={this.props.handleGetStartedClick} className="btn btn-primary">Get Started</button>
+            <button type="button" onClick={props.handleGetStartedClick} className="btn btn-primary">Get Started</button>
           </Link>
         </p>
       </div>
